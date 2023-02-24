d84443fa449f454c6080c9a13173ce9f12d0607f
Oscar Garza <ogarza2881@gmail.com>
2023-02-24 16:03:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -11,34 +11,47 @@ const btnDrawCard = document.querySelector('.btn--draw-card');
 //////////////////////////////////////////////////Set initial Values
 cardEl.classList.add('hidden');
 document.querySelector('.name').textContent = '';
+const dealtCards = [];
+
+function clearArray(array) {
+  while (array.length > 0) {
+    array.pop();
+  }
+}
+
+function generateCard() {
+  document.querySelector('.name').textContent = '';
+  // 1. Generate random dice roll between 1-6
+  let card = Math.trunc(Math.random() * 19) + 1;
+  if (!dealtCards.includes(card)) {
+    dealtCards.push(card);
+    cardEl.classList.remove('hidden');
+    cardEl.src = `card-${card}.png`;
+  } else {
+    if (dealtCards.length !== 19) {
+      generateCard();
+    } else {
+      cardEl.classList.add('hidden');
+      document.querySelector('.name').textContent = 'Game Over';
+    }
+  }
+  console.log(dealtCards);
+}
 
 //////////////////////////////////////////////////// Event Handlers
 // Roll Dice functionality
-btnDrawCard.addEventListener('click', () => {
-  document.querySelector('.name').textContent = '';
-  // 1. Generate random dice roll between 1-6
-  const card = Math.trunc(Math.random() * 19) + 1;
-  // 2. Display dice
-  cardEl.classList.remove('hidden');
-  cardEl.src = `card-${card}.png`;
-  // 3. Check for rolled 1
-});
+// btnDrawCard.addEventListener('click', generateCard);
 
-btnDrawCard.addEventListener('touchstart', () => {
-  document.querySelector('.name').textContent = '';
-  // 1. Generate random dice roll between 1-6
-  const card = Math.trunc(Math.random() * 8) + 1;
-  // 2. Display dice
-  cardEl.classList.remove('hidden');
-  cardEl.src = `card-${card}.png`;
-});
+btnDrawCard.addEventListener('touchstart', generateCard);
 
-btnNew.addEventListener('click', () => {
-  cardEl.classList.add('hidden');
-  document.querySelector('.name').textContent = '';
-});
+// btnNew.addEventListener('click', () => {
+//   cardEl.classList.add('hidden');
+//   document.querySelector('.name').textContent = '';
+//   clearArray(dealtCards);
+// });
 
 btnNew.addEventListener('touchstart', () => {
   cardEl.classList.add('hidden');
   document.querySelector('.name').textContent = '';
+  clearArray(dealtCards);
 });
